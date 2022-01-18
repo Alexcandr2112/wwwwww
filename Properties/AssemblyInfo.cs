e3827ed0ec979d29fd70a7c713f643b8e9750e1b
Alexcandr2112 <86944054+Alexcandr2112@users.noreply.github.com>
2022-01-18 08:45:07 +0300

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("A Discord Rich Presence extension for Visual Studio 2017 and 2019")]
+[assembly: AssemblyDescription("A Discord Rich Presence extension for Visual Studio 2017 and 2019")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("1TheNikita")]
+[assembly: AssemblyProduct("Discord Rich Presence VS extension")]
+[assembly: AssemblyCopyright("Copyright 1TheNikita 2021")]
+[assembly: AssemblyTrademark("1TheNikita")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
